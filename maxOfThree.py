3c2a57c61b61094e4269f6ef9435142dc9a00fb9
Harsh2594 <harsh.sgm13@gmail.com>
2024-12-12 18:08:27 +0530

    use built-in function

--- maxOfThree.py
+++ maxOfThree.py
@@ -3,29 +3,9 @@ a = int(input("Enter first number a = "))
 b = int(input("Enter second number b = "))
 c = int(input("Enter third number c = "))
 
-def max(x,y,z):
-  if a>b:
-    if a>c:
-      print("Max of three number is ",a) 
-    else:
-      print("Max of three number is ",c) 
-  else:
-    if b>c:
-      print("Max of three number is ",b)
-    else:
-      print("Max of three number is ",c) 
-  
-def min(x,y,z):
-  if a<b:
-    if a<c:
-      print("min of three is ",a)
-    else:
-      print("Min of three is ",c)
-  else:
-    if b<c:
-      print("min of three is ",b)
-    else:
-      print("min of three is ",c)  
+#use built-in Python functions for simplicity:
+maximum = max(a,b,c)
+minimum = min(a,b,c)
 
-max(a,b,c)
-min(a,b,c)
+print("max of three numbers is ",maximum)
+print(f"min of three numbers is {minimum}")
