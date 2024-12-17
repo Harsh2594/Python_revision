85a637129071802bb64cd2465663a2b1ae02b89a
Harsh2594 <harsh.sgm13@gmail.com>
2024-12-17 18:18:46 +0530

    concise the code

--- stringreverse.py
+++ stringreverse.py
@@ -1,12 +1,5 @@
 st = input("Enter any string which you want to reverse ")
-lst = list()
-for s in st:
-  lst.append(s)
-print(lst)  
-st1 = str()
-n = len(lst)
-while n > 0:
-  st1 = st1 + lst[n-1]
-  n = n-1
-
+st1 = ""
+for c in st:
+  st1 = c + st1
 print(f"Reverse string is {st1}")  
