a6ea8ad30dfbf49fa8ab97a240be480ebc5578f7
Harsh2594 <harsh.sgm13@gmail.com>
2025-01-06 15:12:49 +0530

    add some more apttern

--- pattern.py
+++ pattern.py
@@ -31,3 +31,23 @@ for i in range(5):
   for j in range(1,i+1):
     print(j,end=" ")
   print()    
+
+print("-----------up traingle------------------")
+#up traingle
+
+for i in range(1,6):
+  for j in range(6-i-1):
+    print(" ",end=" ")
+  for k in range(2*i-1):
+    print("*",end=' ')  
+  print()  
+
+print("------------down traingle-----------------")
+#down traingle
+
+for i in range(5,0,-1):
+    for j in range(5-i):
+        print(" ",end=" ")
+    for k in range(2*i-1):        
+        print("*",end=" ")
+    print()  
