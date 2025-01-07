9c12a5368a3c7fdca1d5ac47d6e9bae8d0c7088b
Harsh2594 <harsh.sgm13@gmail.com>
2025-01-07 15:51:51 +0530

    Implement title case string

---
+++ TitleCaseString.py
@@ -0,0 +1,20 @@
+#Title Case String:
+#INPUT: harsh Pratap singh
+#Output: Harsh Pratap Singh
+
+my_string = input("Enter a string ")
+my_string_list = [(e) for e in my_string]
+n = len(my_string_list)
+my_string_titleCase = ""
+for i in range(n):
+  if i == 0:
+    my_string_list[i] = my_string_list[i].upper()
+  elif my_string_list[i] == " ":
+    my_string_list[i+1] = my_string_list[i+1].upper()
+  else:
+    pass
+
+for e in my_string_list:
+  my_string_titleCase = my_string_titleCase+e
+
+print(my_string_titleCase)
