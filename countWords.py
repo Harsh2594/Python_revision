9c70e753ba5ad53de53759b2c2dac0860a452c8b
Harsh2594 <harsh.sgm13@gmail.com>
2025-01-08 17:19:40 +0530

    Make more effective code

--- countWords.py
+++ countWords.py
@@ -6,13 +6,16 @@ def countWords(text):
   word = ""
   i = 0
   count = 0
-  while i < length:
-    if text[i] != " ":
-      word += text[i]
-    else:
-      if len(word)>1:
-        count += 1
-    i += 1    
-  return count+1        
+  if length > 0:
+    while i < length:
+      if text[i] != " ":
+        word += text[i]
+      else:
+        if len(word)>1:
+          count += 1
+      i += 1    
+    return count+1 
+  else:
+    return 0         
 
 print(f"Number of words in given sentence are {countWords(my_string)}")  
