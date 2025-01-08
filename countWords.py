eb9aa3af7de0f759967de4794002cf4d333d9557
Harsh2594 <harsh.sgm13@gmail.com>
2025-01-08 17:16:25 +0530

    Make method to count words in a sentence

---
+++ countWords.py
@@ -0,0 +1,18 @@
+#Count Words in a Sentence:
+my_string = input("Enter a Sentence String ")
+
+def countWords(text):
+  length = len(my_string)
+  word = ""
+  i = 0
+  count = 0
+  while i < length:
+    if text[i] != " ":
+      word += text[i]
+    else:
+      if len(word)>1:
+        count += 1
+    i += 1    
+  return count+1        
+
+print(f"Number of words in given sentence are {countWords(my_string)}")  
